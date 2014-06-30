29677a8718f619e78d7835eac2237cb91a5368be
Katharina Hayer <katharinaehayer@gmail.com>
2014-06-30 13:37:14 -0400

    added gitignore

--- chocomilk.py
+++ chocomilk.py
@@ -13,13 +13,12 @@ CV_CAP_PROP_FRAME_COUNT = 7
 CV_CAP_PROP_FORMAT = 8
 CV_CAP_PROP_MODE = 9
 
+
 class Target:
 
     def __init__(self,video,background=None,xls_sheet=None):
         self.sample_name = video.split("/")[-1]
         self.capture = cv2.VideoCapture(video)
-        if background:
-          self.background = cv2.LoadImageM(background)
         self.numframes = self.capture.get(CV_CAP_PROP_FRAME_COUNT)
         print >> sys.stderr, self.numframes
         self.fps = self.capture.get(CV_CAP_PROP_FPS)
@@ -28,16 +27,23 @@ class Target:
         if self.increaser == 0:
             self.increaser = 1
         self.length_cali = 250
-        self.size = cv.GetSize(cv.QueryFrame(self.capture))
-        self.background = self.get_background(video)
-        self.open_arm = self.get_open_arm()
-        self.closed_arm = self.get_closed_arm()
-        self.zero_maze = self.get_zeromaze()
-        self.mouse_area = self.get_mouse_area()
-        self.conversion = 19.75
-        print >> stderr, self.mouse_area
+        self.width = self.capture.get(CV_CAP_PROP_FRAME_WIDTH)
+        self.height = self.capture.get(CV_CAP_PROP_FRAME_HEIGHT)
         if xls_sheet:
             self.xls_sheet = xls_sheet
+        if background:
+            self.background = cv2.LoadImageM(background)
+        else:
+            self.background = self.get_background()
+        cv2.imwrite('background.png',self.background)
+
+
+    def get_background(self):
+        self.capture.set(CV_CAP_PROP_POS_FRAMES,int(self.fps*10))
+        _,background = self.capture.read()
+
+        return background
+
 
 def main():
     """Main entry point for the script."""
